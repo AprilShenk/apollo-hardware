19966ffa0fafb22d4c379f7a89f2e9d726ea0048
msanaya <50601483+msanaya@users.noreply.github.com>
2020-09-25 17:00:32 -0400

    Did header with some CSS

--- client/src/components/shared/Header/Header.jsx
+++ client/src/components/shared/Header/Header.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Header.css";
 
-const Header = () => {
+function Header(props) {
+
+  const { input, handleChange } = props
   return (
-    <div>
-      This is Header
+    <div className="header">
+      <div className="left-side">
+        <Link className="header-links" id="header-title" to="/">
+          <img className="attributes" id="header-logo" src="https://storage.googleapis.com/p3images/Logos/logo_black_header.png" alt="Apollo Hardware Logo"></img>
+        </Link>
+        <Link className="header-links" id="header-posts" to="/posts">SHOP</Link>
+        <Link className="header-links" id="header-add-post" to="/add-post">ADMIN</Link>
+      </div>
+      <div className="right-side">
+        <Link className="header-search" to="/products/search-results">
+          <form className="search-form">
+            <input className="search-input" name="Search" type='text' value={input} onChange={handleChange} placeholder='Search Product' />
+            <button type="submit"><i class="fa fa-search"></i></button>
+          </form>
+        </Link>
+      </div>
     </div>
-  );
-};
+
+
+  )
+}
 
 export default Header;
