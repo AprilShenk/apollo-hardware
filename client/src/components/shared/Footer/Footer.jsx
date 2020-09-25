6140c33fd294b97878212adcb27853fa392225fa
Andres Mora <andres@Andress-MacBook-Pro.local>
2020-09-25 17:07:19 -0400

    footer 50%

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import "./Footer.css"
 
 const Footer = () => {
   return (
     <div>
-      This is Footer
+      <footer className="bar">
+        <div className="data1">1855 Forced Labor Way</div>
+        <div className="data2">
+          <img id="logoF" src="https://storage.googleapis.com/p3images/Logos/logo_white_footer.png"></img>
+          <p>Copyright</p>
+        </div>
+        <div className="data3">1(555)382-5347</div>
+      </footer>
     </div>
   );
 };
