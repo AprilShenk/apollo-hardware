34ee8a1ddd88791f50a6354b735b60ae07ba8845
msanaya <50601483+msanaya@users.noreply.github.com>
2020-09-27 00:41:16 -0400

    Finished welcome container

--- client/src/components/WelcomeContainer/WelcomeContainer.jsx
+++ client/src/components/WelcomeContainer/WelcomeContainer.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import "./WelcomeContainer.css";
+import { Link } from 'react-router-dom';
 
 const WelcomeContainer = () => {
   return (
-    <div>
-      <h1>Welcome Container</h1>
+    <div className="welcome-container">
+      <div className="welcome-left-side">
+        <h1 className="welcome-title">WELCOME TO APOLLO HARDWARE</h1>
+        <h2 className="welcome-subtitle">We rise about your competition</h2>
+        <p className="welcome-p">At Apollo Hardware we have been helping customers for 666 years make it to the moon and back with their project ideas. Help us inspire you to do more on your next project</p>
+        <Link to="/add-product">
+        <button id="add-product" type="submit" formaction="/add-product">ADD PRODUCT</button>
+        </Link>
+      </div>
+      <div className="welcome-right-side">
+      <img className="attributes" id="homepage-img" src="https://storage.googleapis.com/p3images/HomePage/original_size_hero_image.jpg" alt="Not available"></img>
+      </div>
+      
     </div>
   );
 };
