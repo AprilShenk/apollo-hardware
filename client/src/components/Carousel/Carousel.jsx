41271b48cfbde17d214a028abab42b23c9fa05c0
Matthew McKnight <matthewmcknight@Matthews-MBP.attlocal.net>
2020-09-28 08:27:40 -0500

    morning push

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -5,8 +5,9 @@ import "./Carousel.css";
 
 const Carousel = () => {
   const [allProducts, setAllProducts] = useState([]);
-  const [curr, setCurr] = useState(0)
-  
+  const [displayCurr, setDisplayCurr] = useState(true);
+  const [myIndex, setMyIndex] = useState(0);
+
   useEffect(() => {
     const fetchProducts = async () => {
       const products = await getProducts();
@@ -15,16 +16,50 @@ const Carousel = () => {
     fetchProducts();
   }, []);
 
-  const carouselJSX = allProducts.map((product, index) => (
-    <CarouselCard
-      _id={product._id}
-      name={product.name}
-      imgURL1={product.imgURL1}
-      key={index}
-    />
-  ));
-  
-  return <div className="carousel">{carouselJSX}</div>;
+  /*Thoughts on what to do - possible fun pair coding exercise -
+  turn allProducts into array of arrays with max of 5 indexes each, 
+  (possibly use recursive call used in shared elements algo to make array of arrays)
+  use set interval to change index of array of arrays to display next index causing inner map to rerun and display items
+  set boundries to once interval kicks at end of outer array it goes back to first index
+   */
+  // const displayArr = ;
+
+  const carousel = () => {
+    for (let i = 0; i < allProducts.length / 5; i++) {
+      setDisplayCurr(false);
+      // x[i].style.display = "none";
+    }
+    setMyIndex(myIndex++);
+    if (myIndex > allProducts.length) {
+      myIndex = 1;
+    }
+    // allProducts[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+  };
+
+  const carouselJSX = allProducts.map((product, index) => {
+    if (index < 5) {
+      return (
+        <div>
+          <CarouselCard
+            _id={product._id}
+            name={product.name}
+            imgURL1={product.imgURL1}
+            key={index}
+          />
+        </div>
+      );
+    }
+  });
+
+  return (
+    <div
+      className="mySlide"
+      style={displayCurr ? { display: `flex` } : { display: "none" }}
+    >
+      {carouselJSX}
+    </div>
+  );
 };
 
 export default Carousel;
