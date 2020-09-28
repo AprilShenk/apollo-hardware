ac7d25a09cf072cc7e092315f4d5578f4e3bd364
Andres Mora <andres@Andress-MacBook-Pro.local>
2020-09-28 02:47:39 -0400

    prod-detail-30%

--- client/src/screens/ProductDetail/ProductDetail.jsx
+++ client/src/screens/ProductDetail/ProductDetail.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import Layout from "../../components/shared/Layout/Layout";
 import { deleteProduct, getProduct } from '../../services/products';
+import "./ProductDetail.css"
 
 const ProductDetail = () => {
 
@@ -26,14 +27,24 @@ const ProductDetail = () => {
     <Layout>
       <div className="product-detail">
         {/* TODO: replace img with DetailCarousel */}
-        <img src={product.imgURL1} alt={product.name}/>
-        <h2>{product.name}</h2>
-        <h3>{product.rating}</h3>
-        <p>{product.description}</p>
-        <p>Price: ${product.price}</p>
-        <p>Qty: {product.quantity}</p>
-        <button className="edit-button"><Link className="edit-link" to={`/products/${product._id}/edit`}>Edit</Link></button>
-        <button className="delete-button" onClick={() => deleteProduct(product._id)}>Delete</button>
+        
+        <div>
+          <div classname="imgSflex1"><img id="imgSmall"  src={product.imgURL1} alt={product.name} /></div>
+          <div classname="imgSflex2"><img id="imgSmall"  src={product.imgURL1} alt={product.name} /></div>
+          <div classname="imgSflex3"><img id="imgSmall"  src={product.imgURL1} alt={product.name} /></div>
+        </div>
+        <div className="imgCont">
+          <div classname="imgBflex"><img id="imgBig" src={product.imgURL1} alt={product.name} /></div>
+        </div>
+        <div className="prodInfo">
+          <h2>{product.name}</h2>
+          <h3>{product.rating}</h3>
+          <p>{product.description}</p>
+          <p>Price: ${product.price}</p>
+          <p>Qty: {product.quantity}</p>
+          <button className="edit-button"><Link className="edit-link" to={`/products/${product._id}/edit`}>Edit</Link></button>
+          <button className="delete-button" onClick={() => deleteProduct(product._id)}>Delete</button>
+        </div>
       </div>
     </Layout>
   );
