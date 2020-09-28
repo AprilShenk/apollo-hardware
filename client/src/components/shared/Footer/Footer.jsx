44a6e87e9ce4f437c0e1b61e71034e0f9d9b7deb
Andres Mora <andres@Andress-MacBook-Pro.local>
2020-09-27 21:47:25 -0400

    footer100%

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -7,8 +7,11 @@ const Footer = () => {
       <footer className="bar">
         <div className="data1">1855 Forced Labor Way</div>
         <div className="data2">
-          <img id="logoF" src="https://storage.googleapis.com/p3images/Logos/logo_white_footer.png"></img>
-          <p>Copyright</p>
+          <a href="https://www.google.com" target="blank">
+            <img id="logoF" src="https://storage.googleapis.com/p3images/Logos/logo_white_footer.png" alt="Apollo H. Logo"></img>
+            {/* <br />Copyright */}
+          </a>
+          <p>Copyright 2069-2420</p>
         </div>
         <div className="data3">1(555)382-5347</div>
       </footer>
